5d97bf520ada2dd74862c0025eef9112ebda428a
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-07-06 19:54:27 +0530

    practice

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// //var let const
+//var let const
 
-// //we wont use var 
-// //let and const are block scoped
-// let x = 10; 
-// const y = 20; // cannot be reassigned
-// let z = 30; // can be reassigned
-// console.log(x); // 10
-// console.log(y); // 20  
-// console.log(z); // 30
-// z = 40; // reassigning z        
-// console.log(z); // 40
-// // const y = 50; // this will throw an error because y is a constant and cannot be reassigned
-// // let x = 50; // this will throw an error because x is already declared in the same scope
+//we wont use var 
+//let and const are block scoped
+let x = 10; 
+const y = 20; // cannot be reassigned
+let z = 30; // can be reassigned
+console.log(x); // 10
+console.log(y); // 20  
+console.log(z); // 30
+z = 40; // reassigning z        
+console.log(z); // 40
+// const y = 50; // this will throw an error because y is a constant and cannot be reassigned
+// let x = 50; // this will throw an error because x is already declared in the same scope
 
 // //declaration and initialization
 // let a; // declaration
