16ae39d6bd8f10dc44c8ff44d560635c5c0632c9
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-09-24 18:12:39 +0530

    practice

--- script2.js
+++ script2.js
@@ -22,3 +22,18 @@ const memoFib = memoize(fibonacci);
 console.log(memoFib(40)); // Much faster than plain fibonacci(40)
 
 
+//custom
+Function.prototype.myBind = function (context, ...args) {
+  const fn = this;
+  return function (...newArgs) {
+    return fn.apply(context, [...args, ...newArgs]);
+  };
+};
+
+// Example
+function greet(greeting, name) {
+  return `${greeting}, ${name}`;
+}
+
+const greetHello = greet.myBind(null, "Hello");
+console.log(greetHello("Alice")); // "Hello, Alice"
