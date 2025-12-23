c6ebea35a36517dcd9da16f49b8e6a22b6598065
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-12-24 00:52:39 +0530

    mousemove practice

--- becomedeveloper.js
+++ becomedeveloper.js
@@ -149,12 +149,106 @@
 
 
 
-let form = document.querySelector("form")
-let inputs = document.querySelectorAll("input")
 
-form.addEventListener("submit", function(e) {
-   e.preventDefault();
 
-   let card = document.createElement("div")
-   card.classList.add("card")
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let form = document.querySelector("form")
+// let inputs = document.querySelectorAll("input")
+// let main = document.querySelector("#main")
+
+// form.addEventListener("submit", function(e) {
+//    e.preventDefault();
+
+//    let card = document.createElement("div");
+//    card.classList.add("card")
+
+//    let profile = document.createElement("div");
+//    profile.classList.add("profile")
+
+//    let img = document.createElement("img");
+//    img.setAttribute("src", inputs[0].value);
+
+//    let h3 = document.createElement("h3");
+//    h3.textContent = inputs[1].value;
+
+//    let h5 = document.createElement("h5")
+//    h5.textContent = inputs[2].value
+
+//    let p = document.createElement("p");
+//    p.textContent = inputs[3].value
+
+
+//    profile.appendChild(img)
+
+//    card.appendChild(profile)
+
+//    card.appendChild(h3)
+
+//    card.appendChild(h5)
+
+//    card.appendChild(p)
+
+//   main.appendChild(card)
+
+//  inputs.forEach(function(e) {
+//    if (e.type !== "submit") {
+//       e.value = "";
+//    }
+//  })
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//mouseover mouseout mousemove
+
+// let div = document.querySelector("#abcd")
+
+// div.addEventListener("mouseover", function() {
+//    div.style.backgroundColor = "yellow"
+// })
+
+// div.addEventListener("mouseout", function(){
+//    div.style.backgroundColor = "red"
+// })
+
+// window.addEventListener("mousemove", function(e) {
+//    div.style.top = e.clientY + "px";
+//    div.style.left = e.clientX + "px"
+// })
