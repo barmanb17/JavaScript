bf2948c84dd6529a8822c0cdc6b875f7f0456d59
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-12-25 20:43:56 +0530

    practice

--- part3.js
+++ part3.js
@@ -296,39 +296,90 @@
 
 //practice of this
 
+// const container = document.querySelector(".users")
+
+// let users = [];
+
+// function addUser(name, role, bio, photo) {
+//     users.push({
+//         name: name,
+//         role: role,
+//         bio: bio,
+//         photo: photo,
+//     })
+// }
+
+// function removeUser(index) {
+//     users.splice(index, 1)
+// }
+
+
+
+
+// users.forEach(function(user) {
+//     const card = document.createElement("div");
+//     const h3 = document.createElement("h3");
+//     h3.textContent = user.name;
+//     card.appendChild(h3);
+
+//     const role = document.createElement("p");
+//     role.textContent = user.role;
+//     card.appendChild(role);
+
+//     const bio = document.createElement("p");
+//     bio.textContent = user.bio;
+//     card.appendChild(bio);
+// })
+
+// container.appendChild(card)
+
 const container = document.querySelector(".users")
 
 let users = [];
-
-function addUser(name, role, bio, photo) {
+function addUser(name, role, bio,) {
     users.push({
         name: name,
         role: role,
         bio: bio,
-        photo: photo,
-    })
+        
+    });
+    renderUsers();
 }
 
 function removeUser(index) {
     users.splice(index, 1)
+    renderUsers();
 }
 
 
-
-
-users.forEach(function(user) {
+function renderUsers(){
+    container.innerHTML = "";
+    users.forEach(function(user, index){
     const card = document.createElement("div");
-    const h3 = document.createElement("h3");
-    h3.textContent = user.name;
-    card.appendChild(h3);
+    const userName = document.createElement("h3");
+    userName.textContent = user.name;
+    card.appendChild(userName);
+
 
     const role = document.createElement("p");
     role.textContent = user.role;
     card.appendChild(role);
 
+
     const bio = document.createElement("p");
     bio.textContent = user.bio;
-    card.appendChild(bio);
-})
+    card.appendChild(bio)
 
-container.appendChild(card)
+    
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", function() {
+        removeUser(index);
+    })
+
+    card.appendChild(deleteBtn);
+
+    container.appendChild(card);
+})
+}
